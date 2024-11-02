ae83d058588934f510e17deade39106286b9dc7c
Saranya <saranyasarathy7@gmail.com>
2024-11-02 22:01:07 +0530

    User Page Added

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -59,3 +59,34 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
             }
       `
 )
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(
+      `
+            *[_type == 'founder' && _id == $id][0] {
+                  _id,
+                  id,
+                  name,
+                  username,
+                  image,
+                  email,
+                  bio
+            }
+      `
+)
+
+export const STARTUPS_BY_AUTHOR = defineQuery(
+      `*[_type == 'startup' && founder._ref == $id] | order(_createdAt desc) {
+            _id,
+            title,
+            slug,
+            _createdAt,
+            author -> {
+                  _id, name, image, bio
+            },
+
+            views,
+            description,
+            category,
+            image
+      }`
+)
